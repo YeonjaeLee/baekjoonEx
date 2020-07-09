2ce75ec4ac81c2b3d7d3e49d381d1c6b33bf5b4c
Yeon_Home <yeonjae115@gmail.com>
2020-07-09 16:11:24 +0900

    [j]Unit01_Ex02

---
+++ java/baekjoonEx/src/com/yeon/unit01/Ex02.java
@@ -0,0 +1,10 @@
+package com.yeon.unit01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        for(int i=0; i<2; i++)
+        {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
